e75e5bc43231ecb90e0e89a12550deea7142f39d
damirusso95 <damirusso95@gmail.com>
2024-05-23 18:25:46 +0200

    M2: array e v-for per stampare i links footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,11 +1,73 @@
 <script>
+export default {
+    data() {
+        return {
+            // ARRAY LINKS NAVBAR
+            linksFooter: [
+                "Characters",
+                "Comics",
+                "Movies",
+                "Tv",
+                "Games",
+                "Videos",
+                "News"
+            ],
 
+        }
+
+    }
+}
 </script>
 
 <template>
-  <h2>prova componente footer</h2>
+    <footer>
+        <div class="container-fluid">
+            <div class="row">
+
+                <div class="col-2">
+                    <h2>DC comics</h2>
+                    <ul v-for="linkFooter in linksFooter">
+                        <li>
+                            {{ linkFooter }}
+                        </li>
+                    </ul>
+
+                </div>
+                <div class="col-2">
+                    <h2>DC</h2>
+                    <ul v-for="linkFooter in linksFooter">
+                        <li>
+                            {{ linkFooter }}
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-2">
+                    <h2>Sites</h2>
+                    <ul v-for="linkFooter in linksFooter">
+                        <li>
+                            {{ linkFooter }}
+                        </li>
+                    </ul>
+                </div>
+
+
+            </div>
+        </div>
+    </footer>
 </template>
 
-<style>
+<style scoped>
+footer {
+    color: aliceblue;
+}
 
+.container-fluid {
+    background-image: url(../assets/img/footer-bg.jpg);
+    height: 350px;
+}
+
+ul {
+    list-style: none;
+    padding: 0;
+}
 </style>
